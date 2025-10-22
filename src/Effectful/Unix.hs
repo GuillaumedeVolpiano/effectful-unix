62fcbb666e7a92084ec34ef3c262c1c2782c73ab
GuillaumedeVolpiano <xavier@wheredoibegin.fr>
2025-10-22 11:16:27 +0200

    Adding fileSize

--- src/Effectful/Unix.hs
+++ src/Effectful/Unix.hs
@@ -11,28 +11,34 @@ module Effectful.Unix
   (Unix
   , runUnix
   , fileID
-  , isDirectory
+  , fileSize
   , getDirectoryContents
-  , listDirectory)
+  , isDirectory
+  , listDirectory
+  , FileID
+  , FileOffset)
 where
 import Effectful (Effect, Eff, (:>), IOE, liftIO)
-import System.Posix.Types (FileID)
+import System.Posix.Types (FileID, FileOffset)
 import Effectful.Dispatch.Dynamic (interpret)
 import qualified System.Directory as D (getDirectoryContents, listDirectory)
 import System.Posix.Files (getFileStatus)
-import qualified System.Posix.Files as P (isDirectory, fileID)
+import qualified System.Posix.Files as P (isDirectory, fileID, fileSize)
 import Effectful.TH (makeEffect)
 
 data Unix :: Effect where
   FileID :: FilePath -> Unix m FileID
+  FileSize :: FilePath -> Unix m FileOffset
+  GetDirectoryContents :: FilePath -> Unix m [FilePath]
   IsDirectory :: FilePath -> Unix m Bool
   ListDirectory :: FilePath -> Unix m [FilePath]
-  GetDirectoryContents :: FilePath -> Unix m [FilePath]
+
 makeEffect ''Unix
 
 runUnix :: IOE :> es => Eff (Unix : es) a -> Eff es a
 runUnix = interpret $ \_ -> \case 
                                 FileID path -> liftIO $ P.fileID <$> getFileStatus path
+                                FileSize path -> liftIO $ P.fileSize <$> getFileStatus path
+                                GetDirectoryContents path -> liftIO $ D.getDirectoryContents path
                                 IsDirectory path -> liftIO $ P.isDirectory <$> getFileStatus path
                                 ListDirectory path -> liftIO $ D.listDirectory path
-                                GetDirectoryContents path -> liftIO $ D.getDirectoryContents path
